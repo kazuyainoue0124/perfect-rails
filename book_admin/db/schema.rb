fc56d8374e68c1b145febef88619784f6aca7abd
Kazuya INOUE <bauer24tbp@gmail.com>
2024-06-26 08:50:25 +0900

    Userモデルの作成

--- book_admin/db/schema.rb
+++ book_admin/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_23_234933) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_25_234517) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.string "penname"
@@ -41,6 +41,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_23_234933) do
   create_table "publishers", force: :cascade do |t|
     t.string "name"
     t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
