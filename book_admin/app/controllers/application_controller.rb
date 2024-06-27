55bb848ce8dc0e50d492985344cac353c68de45a
Kazuya INOUE <bauer24tbp@gmail.com>
2024-06-27 09:09:15 +0900

    2-5-1ヘルパーメソッドまで

--- book_admin/app/controllers/application_controller.rb
+++ book_admin/app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action do
-    redirect_to access_denied_path if params[:token].blank?
+  before_action :detect_mobile_variant
+
+  private
+
+  def detect_mobile_variant
+    request.variant = :mobile if request.user_agent =~ /iPhone/
   end
 end
--- book_admin/app/helpers/application_helper.rb
+++ book_admin/app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def to_hankaku(str)
+    # 全角の英文字を半角に変換
+    str.tr('Ａ-Ｚａ-ｚ', 'A-Za-z')
+  end
 end
