0b0ec267f35bf974b93550dbe879f3da6602dca8
Kazuya INOUE <bauer24tbp@gmail.com>
2024-07-11 08:55:22 +0900

    ActionMailerのテストを追加

--- action_mailer_app/test/mailers/user_mailer_test.rb
+++ action_mailer_app/test/mailers/user_mailer_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class UserMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "welcome" do
+    email = UserMailer.with(to: "igarashi@example.com", name: "igaiga").welcome
+    # メールが送信キューに追加されるかどうかをテスト
+    assert_emails(1){ email.deliver_now }
+    # 送信されたメールを確認するテスト
+    assert_equal ["perfect_rails@example.com"], email.from
+    assert_equal ["igarashi@example.com"], email.to
+    assert_equal "登録完了", email.subject
+    assert_includes email.text_part.decoded, "igaiga"
+    assert_includes email.html_part.decoded, "igaiga"
+  end
 end
