c1fb02c320e0fe0871aae6741efbd8b9ecb6d33c
Kazuya INOUE <bauer24tbp@gmail.com>
2024-07-15 10:26:20 +0900

    サンプルアプリケーションの実装

--- awesome_events/config/initializers/omniauth.rb
+++ awesome_events/config/initializers/omniauth.rb
@@ -1,10 +1,14 @@
+OmniAuth.config.allowed_request_methods = [:post, :get]
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development? || Rails.env.test?
-    provider :github, 'Ov23limJQaVDkYCWaRuP', '50d5d81792966f0532a1b5401d179c74ae569c75'
+    provider :github, 'Ov23limJQaVDkYCWaRuP', '50d5d81792966f0532a1b5401d179c74ae569c75', callback_path: '/auth/github/callback'
   else
     provider :github,
       Rails.application.credentials.github[:client_id],
       Rails.application.credentials.github[:client_secret]
   end
+
+  OmniAuth.config.allowed_request_methods = [:post]
 end
 
--- awesome_events/config/routes.rb
+++ awesome_events/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get '/auth/:provider/callback', to: 'sessions#create'
+  post '/auth/github', to: lambda { |_env| [404, {}, ['Not Found']] }
 end
